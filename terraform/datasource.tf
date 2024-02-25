953277c575d691a639119d667090d77422619a97
Serguei Tokovenko <docsvt@gmail.com>
2024-02-25 21:40:54 +0000

    Fix folder id for image

--- terraform/datasource.tf
+++ terraform/datasource.tf
@@ -1,5 +1,6 @@
 data "yandex_compute_image" "this" {
   name = "${var.image_base_name}-${var.image_tag}"
+  folder_id = data.yandex_resourcemanager_folder.this.id
 }
 
 data "yandex_resourcemanager_folder" "this" {
